91203ba94014c86fccae75e7b4be1835a02e53fb
BananaOnTheTree <124771692+BananaOnTheTree@users.noreply.github.com>
2024-12-10 08:02:37 +0700

    Serializable

--- app/src/main/java/com/example/flybooking/model/TransferLocInfo.kt
+++ app/src/main/java/com/example/flybooking/model/TransferLocInfo.kt
@@ -1,12 +1,15 @@
 package com.example.flybooking.model
 
 import com.example.flybooking.model.response.amadeus.GeoCode
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class TransferLocInfo(
     val addressLine: String = "addressLine",
     val geoCode: GeoCode,
 )
 
+@Serializable
 data class TransferInfo(
     val from: TransferLocInfo,
     val to: TransferLocInfo,
